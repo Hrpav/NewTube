ef64b5810d7b299243bd4167fe3f549787d3c922
Hrpavi <scstruxton@gmail.com>
2026-04-02 14:07:23 +0200

    t

--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,11 +1,17 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
-import { MainSection } from "./main-section"
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { MainSection } from "./main-section";
+import { PersonalSection } from "./personal-section";
 
 export const HomeSidebar = () => {
     return (
-        <Sidebar className="pt-16 z-40 border-none">
+        <Sidebar className="pt-16 z-40 border-none"> 
             <SidebarContent className="bg-background">
                 <MainSection />
+                <Separator className="bg-gray-200 dark:bg-gray-800 my-2" />
+                <PersonalSection />
             </SidebarContent>
         </Sidebar>
     )

0cf91d0587325d6e576c83684ed352bce735f5fa
Hrpavi <scstruxton@gmail.com>
2026-04-02 11:04:56 +0200

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      <Button>
-        Click me
-        </Button>
+      <Image src="/logo.svg" height={50} width={50} alt="Logo" />
     </div>
   )
 }

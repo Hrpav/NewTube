4510ac9a2ff3a16184c55c5c2c99c89d05dec27c
Hrpavi <scstruxton@gmail.com>
2026-04-02 15:07:54 +0200

    add testing for /protected and /protected/subroute, will display message if the user is logged in, elsewhere will put the login screen

--- src/app/(home)/protected/subroute/page.tsx
+++ src/app/(home)/protected/subroute/page.tsx
@@ -1,11 +1,11 @@
-// testing for http://localhost:3000/protected/
+// testing for http://localhost:3000/protected/subroute
 // if logged in, should say the message
 // else will go to login/up screen
 
 const Page = () => {
     return (
         <div>
-            Only authorized users should see this
+            Only authorized users should see this subroute
         </div>
     );
 };

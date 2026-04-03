926e417af522789e367cfef7d635c9da014923ef
Hrpavi <scstruxton@gmail.com>
2026-04-03 13:11:07 +0200

    create trpc examples and the loading and error screen

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,26 @@
 import { HydrateClient, trpc } from "@/trpc/server";
 import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 // im not sure if you need async on the void trpc one?
 // but if i remove async its not gonna throw any errors/warnings
 // ill use async everywhere lol
 export default async function Home() {
-//  const data = await trpc.hello({ text: "Hrpavi" }); // SERVER COMPONENT FETCHING FROM THE DATA
-                                                     // WITHOUT "use client";
-    void trpc.hello.prefetch({ text: "Hrpavi" });
+  // i think there are more than one buti used this one...
+  // const data = await trpc.hello({ text: "Hrpavi" }); // SERVER COMPONENT FETCHING FROM THE DATA
+                                                         // WITHOUT "use client";
+  void trpc.hello.prefetch({ text: "Hrpavi" });
 
   return (
     <div>
       <HydrateClient>
-        <PageClient />
+        <Suspense fallback={<p>Loading...</p>}>
+          <ErrorBoundary fallback={<p>Error!</p>}>
+            <PageClient />
+          </ErrorBoundary>
+        </Suspense>
       </HydrateClient>
     </div>
   );
-};
+}

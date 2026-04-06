9b065f4a3e11b2956fcee84efec9ed18f576ea66
Hrpavi <scstruxton@gmail.com>
2026-04-06 13:16:12 +0200

    created thumbnails, gif previews when you hover over the video in the /studio and the duration mark (also in /studio)

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -79,7 +79,12 @@ const VideosSectionSuspense = () => {
                     <TableCell className="pl-6 py-4">
                       <div className="flex items-center gap-4">
                         <div className="relative aspect-video w-36 shrink-0">
-                          <VideoThumbnail imageUrl={video.thumbnailUrl} />
+                          <VideoThumbnail 
+                           imageUrl={video.thumbnailUrl} 
+                           previewUrl={video.previewUrl}
+                           title={video.title}
+                           duration={video.duration || 0}
+                          />
                         </div>
                       </div>
                       </TableCell>
--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -1,21 +1,41 @@
-import Image from "next/image"
+import { formatDuration } from "@/lib/utils";
+import Image from "next/image";
 
 interface VideoThumbnailProps {
-    imageUrl?: string | null;
+  imageUrl?: string | null;
+  title: string;
+  previewUrl?: string | null;
+  duration: number;
 }
 
 export const VideoThumbnail = ({
-    imageUrl,
+  imageUrl,
+  title,
+  previewUrl,
+  duration,
 }: VideoThumbnailProps) => {
-    return (
-        <div className="relative">
-            {/* Thumbnail wrapper */}
-            <div className="relative w-full overflow-hidden rounded-xl aspect-video">
-                <Image src={imageUrl ?? "/placeholder.svg"} alt="Thumbnail" fill className="h-full w-full object-cover" />
-            </div>
+  return (
+    <div className="relative">
+      {/* Thumbnail wrapper */}
+      <div className="relative w-full overflow-hidden rounded-xl aspect-video">
+        <Image
+          src={imageUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="h-full w-full object-cover group-hover:opacity-0"
+        />
+        <Image
+          src={previewUrl ?? "/placeholder.svg"}
+          alt={title}
+          fill
+          className="h-full w-full object-cover opacity-0 group-hover:opacity-100"
+        />
+      </div>
 
-            {/* Video duration box */}
-            {/* TODO: Add video duration box */}
-        </div>
-    )
-}
+      {/* Video duration box */}
+      <div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/80 text-white text-xs font-medium">
+        {formatDuration(duration)}
+      </div>
+    </div>
+  );
+};
